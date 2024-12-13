0ad7048a902d37e77ea7f4550f8a76d8c1a92d08
unknown <paradox81ru@yandex.ru>
2024-12-13 12:21:59 +0300

    Modification of the index page to a single-page one

--- OAuth2/ui/routes/html.py
+++ OAuth2/ui/routes/html.py
@@ -14,23 +14,9 @@ router = APIRouter(
     tags=["html"]
 )
 
+
 @router.get("/index")
 async def main(request: Request):
     return templates.TemplateResponse(
         request=request, name="index.html"
-    )
-
-@router.post("/index")
-async def get_tokens(db_session: Annotated[Session, Depends(get_db_session)], request: Request,
-                     form_data: Annotated[FormData, Depends(get_form_data)]):
-    user_manager = UserManager(db_session)
-    jwt_token_manager = JWTTokenManager(db_session)
-    user = user_manager.get_authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    access_token = jwt_token_manager.create_access_token(user.username, data={'scopes': form_data.scope})
-    refresh_token = jwt_token_manager.create_refresh_token(user.username)
-    return templates.TemplateResponse(
-        request=request, name="tokens_show.html", context={"access_token": access_token,"refresh_token": refresh_token,
-                                                           "scopes": form_data.scope}
     )
--- OAuth2/ui/static/index.js
+++ OAuth2/ui/static/index.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const host = window.location.origin;
 
     const formAuth = document.getElementById("formAuth");
+    const alertDanger = document.getElementById("alertDanger");
     const fieldUsername = document.getElementById("fldUsername");
     const fieldPassword = document.getElementById("fldPassword");
     const checkboxScopeMe = document.getElementById("chbScopeMe");
@@ -11,83 +12,114 @@ document.addEventListener('DOMContentLoaded', function() {
     const fieldTokenAccess = document.getElementById("fldTokenAccess");
     const fieldTokenRefresh = document.getElementById("fldTokenRefresh");
     const btnTokenRefresh = document.getElementById("btnTokenRefresh");
+    const HTML_CLASS_HIDDEN = "d-none";
 
-    btnTokenRefresh.addEventListener("click", requestTokenHandler)
+    btnRequestTokens.addEventListener("click", requestTokenHandler);
+    btnTokenRefresh.addEventListener('click', requestRefreshTokenHandler);
 
+    /**
+     * Обработчик кнопик запроса токенов
+     * @param {*} ev 
+     */
     function requestTokenHandler(ev) {
-        authorization();
+        authorizationRequest();
     }
 
-    function authorization() {
+    /**
+     * Обработчик кнопки обновления токенов
+     * @param {*} ev 
+     */
+    function requestRefreshTokenHandler(ev) {
+        tokenRefreshRequest();
+    }
+
+    /**
+     * Запрос полчения токенов
+     */
+    function authorizationRequest() {
         const api = "/api/oauth/token";
         const formData = new FormData(formAuth);
 
-        apiRequest("POST", api, successfulReceiptOfTokens, errorReceiptOfTokens, data=formData);
+        apiRequest("POST", host, api, successfulResponse, errorResponse, {}, formData);
+    }
+
+    /**
+     * Запросо обновления токенов
+     */
+    function tokenRefreshRequest() {
+        const api = "/api/oauth/token-refresh";
+        const tokenRefresh = `bearer ${fieldTokenRefresh.value}`;
+        const headers = {Authorization: tokenRefresh};
+
+        apiRequest("POST", host, api, successfulResponse, errorResponse, headers, {});
     }
 
     /**
      * Функция обработки успешного получения токенов
      * @param {У} data 
      */
-    function successfulReceiptOfTokens(data) {
-        alert(data);
+    function successfulResponse(data) {
+        hideAllertDanger();
+        fillTokenFields(data['access_token'], data['refresh_token'])
     }
+ 
 
     /**
-     * Ошибка при получении токенов
+     * Ошибка при ответе от срвера
      * @param {*} statusText 
      * @param {*} response 
      */
-    function errorReceiptOfTokens(statusText, response) {
-        alert(statusText + " " + response);
+    function errorResponse(statusText, statusCode, response) {
+        clearTokenFields();
+        if ([400, 401].indexOf(statusCode) != -1)
+            response.then(responsen_json => {
+                showAlertDanger(`Error: ${statusText} ${responsen_json['detail']}`);
+        });
+            // showAlertDanger(`Error: ${statusText} ${responsen['detail']}`);
+        else {
+            showAlertDanger(`Error: ${statusCode} - ${statusText}`);
+        }
+    }
+
+    /**
+     * Заполняет поля токенов данными
+     * @param {String} accessToken 
+     * @param {String} refreshToken 
+     */
+    function fillTokenFields(accessToken, refreshToken) {
+        fieldTokenAccess.value = accessToken;
+        fieldTokenRefresh.value =  refreshToken;
+    }
+
+    /**
+     * Очищает поля токенов
+     */
+    function clearTokenFields() {
+        fieldTokenAccess.value = '';
+        fieldTokenRefresh.value = '';
     }
 
 
     /**
-     * API запрос
-     * @param {String method} метод запроса
-     * @param {String} api эндпоинт
-     * @param {function} successCallback функция обратного вызова при успешном ответе
-     * @param {function} failedStatusCallback функция обратного вызова при ошибке ответа
-     * @param {{}} [headers={}] заголовки запроса
-     * @param {{}} [data={}] данные запроса
+     * Отображет сообшение об ошибке
+     * @param {String} msg сообшение об ошибке
      */
-    function apiRequest(method, api, successCallback, failedStatusCallback, headers={}, data={}) {
-        const url=`${host}${api}`;
-        const requiredHeaders = {"content-type": "application/json; charset=UTF-8"};
-        Object.assign(headers, requiredHeaders);
-
-
-        let options = {
-            headers: headers,
-            method: method,
-        };
-
-        // Если переданы доанные формы,
-        if (data instanceof FormData) {
-            // то эту форму передает в тело запроса.
-            options['body'] = data;
-        } else if ((typeof (data) === 'object') && !(isObjectEmpty(data))) {
-            // Иначе эти данные должны быть переданы в свойстве body в виде параметра.
-            options['body'] = getSearchParams(args.data);
-        }
-
-        fetch(url, options)
-        .then(response => {
-            if (response.ok) {
-                return response.json()
-            } else {
-                throw new StatusError(response.statusText, response)
-            }
-        })
-        .then(data => {
-            successCallback(data);
-        })
-        .catch(error => {
-            failedStatusCallback(error.message, error.respons)
-        });
+    function showAlertDanger(msg) {
+        alertDanger.textContent = msg;
+        if (alertDanger.classList.contains(HTML_CLASS_HIDDEN)) 
+            alertDanger.classList.remove(HTML_CLASS_HIDDEN);
     }
 
+    /**
+     * Скрывает сообшение об ошибке
+     */
+    function hideAllertDanger() {
+        if (!alertDanger.classList.contains(HTML_CLASS_HIDDEN)) {
+            alertDanger.classList.add(HTML_CLASS_HIDDEN);
+            alertDanger.textContent = "";
+        }
+
+    }
 });
 
 /**
@@ -115,10 +147,68 @@ function getSearchParams(data) {
     return searchParams.toString();
 }
 
+/**
+ * API запрос
+ * @param {String method} метод запроса
+ * @param {String} api эндпоинт
+ * @param {function} successCallback функция обратного вызова при успешном ответе
+ * @param {function} failedStatusCallback функция обратного вызова при ошибке ответа
+ * @param {{}} [headers={}] заголовки запроса
+ * @param {{}} [data={}] данные запроса
+ */
+function apiRequest(method, host, api, successCallback, failedStatusCallback, headers={}, data={}) {
+    const url=`${host}${api}`;
+    const requiredHeaders = {"content-type": "application/json; charset=UTF-8"};
+    Object.assign(headers, requiredHeaders);
+
+
+    let options = {
+        headers: headers,
+        method: method,
+    };
+
+    // Если переданы доанные формы,
+    if (data instanceof FormData) {
+        delete (options.headers);
+        // то эту форму передает в тело запроса.
+        options['body'] = data;
+    } else if ((typeof (data) === 'object') && !(isObjectEmpty(data))) {
+        // Иначе эти данные должны быть переданы в свойстве body в виде параметра.
+        options['body'] = getSearchParams(data);
+    }
+
+    fetch(url, options)
+    .then(response => {
+        if (response.ok) {
+            return response.json()
+        } else {
+            // throw new ResonseError(response.statusText, response.status, response);
+            throw new StatusError(response.statusText, response.status, response.json())
+        }
+    })
+    .then(data => {
+        successCallback(data);
+    })
+    .catch(error => {
+        // return error.resopnse.json
+       failedStatusCallback(error.message, error.statusCode, error.response)
+    })
+}
+
 class StatusError extends Error {
-    constructor(message, response) {
+    constructor(message, statusCode, response) {
         super(message);
         this.name = "StatusError";
+        this.statusCode = statusCode;
         this.response = response;
     }
 }
+
+// class ResonseError extends Error {
+//     constructor(message, statusCode, resopnse) {
+//         super(message);
+//         this.name = "StatusError";
+//         this.statusCode = statusCode;
+//         this.response = resopnse;
+//     }
+// }
